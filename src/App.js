08649d0b7a6b02fe3c0902bdf8f40eb80a6f0359
Amrendra Nath <cool.amrendrasrivastav@gmail.com>
2022-08-02 13:55:15 +0530

    modify style

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { Box, Container, IconButton } from "@mui/material";
+import { LinkedIn, GitHub } from "@mui/icons-material";
+
+import Typed from "typed.js";
 
 function App() {
+  useEffect(() => {
+    let typed = new Typed(".auto-input", {
+      strings: ["Amrendra Nath.", "Frontend Engineer."],
+      typeSpeed: 100,
+      backSpeed: 100,
+      loop: true,
+    });
+    return () => typed.destroy();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <Box sx={{ width: "100vw", height: "100vh", backgroundColor: "#111", color: "#fff" }}>
+      <Container sx={{ height: "inherit", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
+        <h1 className="text-5xl uppercase font-bold text-[#4169e1]">
+          I'm <span className="auto-input italic"></span>
+        </h1>
+        <p className="my-7 font-medium text-2xl">
+          I'm an Indian based front‑end engineer focused on crafting clean & user‑friendly experiences, <br /> I am passionate about
+          building excellent software that improves the lives of those around me.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <Box className="flex justify-center">
+          <IconButton size="large" color="primary" href="https://github.com/amrendranath">
+            <GitHub fontSize="large" />
+          </IconButton>
+          <IconButton size="large" color="primary" href="https://www.linkedin.com/in/amrendranath">
+            <LinkedIn fontSize="large" />
+          </IconButton>
+        </Box>
+      </Container>
+    </Box>
   );
 }
 
